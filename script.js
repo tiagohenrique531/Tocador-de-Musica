dabf3245a3afa0ae9dadf5075ff86cb2f0ea2a8b
Tiago <109531913+tiagohenrique531@users.noreply.github.com>
2022-09-06 10:32:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+let musicas = [
+    {titulo:'Sweet Child O Mine', artista:'Guns and roses', src:'musicas/Guns.mp3', img:'imagens/sweet.jpg', letras:'Shes got a smile that it seems to me Reminds me of childhood memories Where everything was as fresh as the bright blue sky Now and then when I see her face She takes me away to that special place And if I stare too long, I d probably break down and cry Whoa, oh, oh Sweet child o mine Whoa, oh, oh, oh Sweet love of mine Shes got eyes of the bluest skies As if they thought of rain I d hate to look into those eyes and see an ounce of pain Her hair reminds me of a warm safe place Where as a child Id hide And pray for the thunder and the rain to quietly pass me by Whoa, oh, oh Sweet child o mine Whoa whoa, oh, oh, oh Sweet love of mine Whoa, yeah Whoa, oh, oh, oh Sweet child o mine Whoa, oh, whoa, oh Sweet love of mine Whoa, oh, oh, oh Sweet child o mine Ooh, yeah Ooh, sweet love of mine Where do we go? Where do we go now? Where do we go? Ooh, oh, where do we go? Where do we go now? Oh, where do we go now? Where do we go? (Sweet child) Where do we go now? Ay, ay, ay, ay, ay, ay, ay, ay Where do we go now? Ah, ah Where do we go? Oh, where do we go now? Oh, where do we go? Oh, where do we go now? Where do we go? Oh, where do we go now? Now, now, now, now, now, now, now Sweet child Sweet child of mine'},
+    {titulo:'Ai preto', artista:'L7NNON_BIEL FURDUNCINHO', src:'musicas/L7NNON _ BIEL DO FURDUNCINHO - Ai Preto -- (part. Bianca)(MP3_320K).mp3', img:'imagens/l7 e biel.jpg', letras:'Esse é o pique do Biel Vem! Vou te pegar bolado (não ligo) Quer deixar no sigilo? (Eu deixo) Quer vir pra minha base? (Eu quero) Então entra na nave (ai, para!) Ó, arranhou minhas costas Quando passei com a minha tropa (ainda) 13 no suporte, meu bolso com várias nota Emoji babando na minha foto logo cedo Fala baixin (ai, preto!) Olha pra mim (ai, preto!) Fala meu nome (ai, preto!) Joga pro homem (ai, preto!) Maciçado, trajado, lalá no peito que elas gosta Sabe que a tropa maceta bem Os tralha que tá moda (ainda) Provou uma vez, agora quer o tempo inteiro Fala pra mim (ai, preto!) Sussurra baixin, ahn (ai, preto!) Geme pra mim, ó (ai, preto!) Fala baixin (ai, preto!) Esse é o pique do Biel Vem Vou te pegar bolado (não ligo) Quer deixar no sigilo? (Eu deixo) Quer vir pra minha base? (Eu quero) Então entra na nave (ai, para!) Ó, arranhou minhas costas Quando passei com a minha tropa (ainda) 13 no suporte, meu bolso com várias nota Emoji babando na minha foto logo cedo Fala baixin (ai, preto!) Olha pra mim (ai, preto!) Fala meu nome (ai, preto!) Joga pro homem (ai, preto!) Maciçado, trajado, lalá no peito que elas gosta Sabe que a tropa maceta bem Os tralha que tá moda (ainda) Provou uma vez, agora quer o tempo inteiro Fala pra mim (ai, preto!) Sussurra baixin, ahn (ai, preto!) Geme pra mim, ó (ai, preto!) Fala baixin (ai, preto!) Esse é o pique do Biel Vem'},
+    {titulo:'INCENDEIA', artista:'Kevin Cris', src:'musicas/encendeia.mp3', img:'imagens/incendeia.jpg', letras:'É-é-é Toda vez que você vem em mim Incendeia, no movimento mantém Cavalgando de quatro Toma tapa na raba De ladin, só botada Essa cara de safada que tá querendo vara Então empina, ah-ah Rebola pra mim, novinha Então empina, ah-ah-ah Rebola pra mim Vai de ladin Então empina, ah-ah Rebola pra mim, novinha Então empina, ah-ah-ah Rebola pra mim Vai de ladin Desce de quatrão, é Faz o movimento que é bom, que é bom Desce de quatrão, é Faz o movimento que é bom, que é bom Coisa linda, bebê Hoje é eu e você Vem, mulher, sentando bem Vem com a tropa do Mantém Vou chamar pra foder na ondinha do MD Sacanagem hoje tem Não tem jeito, nóis é o trem Então empina, ah-ah Rebola pra mim, novinha Então empina, ah-ah-ah Rebola pra mim Vai de ladin Toda vez que você vem em mim Incendeia, no movimento mantém Cavalgando de quatro Toma tapa na raba De ladin, só botada Essa cara de safada que tá querendo vara Então empina, ah-ah Rebola pra mim, novinha Então empina, ah-ah-ah Rebola pra mim Vai de ladin Então empina, ah-ah Rebola pra mim, novinha Então empina, ah-ah-ah Rebola pra mim Vai de ladin Desce de quatrão, é Faz o movimento que é bom, que é bom Desce de quatrão, é Faz o movimento que é bom, que é bom Coisa linda, bebê Hoje é eu e você Vem, mulher, sentando bem Vem com a tropa do Mantém Vou chamar pra foder na ondinha do MD Sacanagem hoje tem Não tem jeito, nóis é o trem Então empina, ah-ah Rebola pra mim, novinha Então empina, ah-ah-ah Rebola pra mim Vai de ladin'},
+    {titulo:'A casa é sua', artista:'Casa Worship', src:'musicas/A casa É Sua.mp3', img:'imagens/a casa é sua.jpg', letras:'Você é bem-vindo, aqui A casa é Sua, pode entrar Me esvazio de mim Me esvazio de mim Sopra o Teu vento aqui Toma o Teu trono vem reinar Nós queremos Te ouvir Nós queremos Te ouvir Essa casa é Sua casa Nós deixamos ela pra Você, Jesus Essa casa é Sua casa Nós deixamos ela pra Você, Jesus Essa casa é Sua casa Nós deixamos ela pra Você, Jesus Essa casa é Sua casa Nós deixamos ela pra Você, Jesus Apareça, que o Teu nome cresça Enche este lugar Enche este lugar Apareça, '},
+    {titulo:'De quem é a culpa', artista:'Marília Mendonça', src:'musicas/00 De Quem É a Culpa_.mp3', img:'imagens/de quem é a culpa.jpg', letras:'Exagerado, sim Sou mais você que eu Sobrevivo de olhares E alguns abraços que me deu E o que vai ser de mim E o meu assunto que não muda? Minha cabeça não ajuda Loucura, tortura E que se dane a minha postura Se eu mudei, você não viu Eu só queria ter você por perto Mas você sumiu É tipo um vício que não tem mais cura E agora, de quem é a culpa? A culpa é sua por ter esse sorriso Ou a culpa é minha por me apaixonar por ele? Só isso Não finja que eu não to falando com você Eu to parado no meio da rua Eu to entrando no meio dos carros Sem você a vida não continua Não finja que eu não to falando com você Ninguém entende o que eu to passando? Quem é você, que eu não conheço mais? Me apaixonei pelo que eu inventei de você Mas você sumiu É tipo um vício que não tem mais cura E agora, de quem é a culpa? A culpa é sua por ter esse sorriso Ou a culpa é minha por me apaixonar por ele? Só isso Não finja que eu não to falando com você Eu to parado no meio da rua Eu to entrando no meio dos carros Sem você a vida não continua Não finja que eu não to falando com você Ninguém entende o que eu to passando Quem é você, que eu não conheço mais? Me apaixonei pelo que eu inventei de você Iê-iê, iê-iê Me apaixonei pelo que eu inventei de você'}
+];
+
+let musica = document.querySelector('audio');
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector('.fim');
+let imagem = document.querySelector('.img');
+let nomeMusica = document.querySelector('.descricao h2');
+let nomeArtista = document.querySelector('.descricao i');
+let letraDaMusica = document.querySelector('.letra h3');
+let barra = document.querySelector('progress');
+let encontra = document.getElementById('encontrar');
+
+
+renderizarMusica(indexMusica);
+
+// Eventos
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra);
+
+document.addEventListener('keypress', precionarEnter);
+
+
+// Voltar para a musica anterior
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if (indexMusica < 0) {
+        indexMusica = 4;
+    }
+    renderizarMusica(indexMusica);
+});
+
+// Passar para a proxima musica
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if (indexMusica > 4){
+        indexMusica = 0;
+    }
+    renderizarMusica(indexMusica);
+});
+
+// Funções
+
+// Função quando a tecla enter for precionada
+function precionarEnter(e) {
+    if(e.key === 'Enter') {
+
+        var btn = document.getElementById("pesquisar");
+
+        btn.click();
+
+        Buscar(btn);
+    }
+} 
+
+// Funçao para buscar
+function Buscar() {
+    let pesquisa = document.getElementById('pesquisar');
+    let procura = pesquisa.value;
+    if (procura == 'Sweet Child O Mine' || procura == 'Guns and roses' ) {
+        index = 0;
+        renderizarMusica(index);
+    } else if(procura == 'Ai preto' || procura == 'ai preto' || procura == 'L7NNON_BIEL FURDUNCINHO' || procura == 'l7 e biel') {
+        index = 1;
+        renderizarMusica(index);
+    } else if(procura == 'INCENDEIA' || procura == 'incendeia' || procura == 'Kevin Cris' || procura == 'kevin cris' || procura == 'kevin') {
+        index = 2;
+        renderizarMusica(index);
+    } else if(procura == 'A casa é sua' || procura == 'a casa é sua' || procura == 'Casa Worship' || procura == 'casa worship') {
+        index = 3;
+        renderizarMusica(index);
+    } else if (procura == 'De quem é a culpa' || procura == 'de quem é a culpa' || procura == 'Marília Mendonça' || procura == 'marília mendonça') {
+        index = 4;
+        renderizarMusica(index);
+    } else {
+        encontra.textContent = 'Sua pesquisa não foi encontrada';
+        encontra.style.color = 'red';
+        renderizarMusica();
+    }
+
+}
+
+
+// função para mudar a musica
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        letraDaMusica.textContent = musicas[index].letras;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+        barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+        document.querySelector('.botao-pause').style.display = 'none';
+        document.querySelector('.botao-play').style.display = 'block';
+        encontra.textContent = '';
+
+    });
+}
+
+// função para tocar a música
+function tocarMusica(){
+    musica.play();
+    document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';
+}
+
+// função pausar a música
+function pausarMusica(){
+    musica.pause();
+    document.querySelector('.botao-pause').style.display = 'none';
+    document.querySelector('.botao-play').style.display = 'block';
+}
+
+// Mexer a barra em rolar da musica
+function atualizarBarra(){
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+
+    // Tempo da música andar em correr da música
+    let tempoDecorrido = document.querySelector('.inicio');
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+}
+
+function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
+}
